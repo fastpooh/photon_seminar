6b9a7b06780853c6e960d4a117e2fd70e20718a3
fastpooh <119751475+fastpooh@users.noreply.github.com>
2023-06-05 01:53:03 +0900

    upload1

--- photonSeminar/Assets/Scripts/PlayerCtrl.cs
+++ photonSeminar/Assets/Scripts/PlayerCtrl.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    Vector3 moveVec;
+    private Vector3 moveVec;
+    private new Transform transform;
+    private float speedController;
 
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
+        speedController = 3f;
     }
 
     // float hAxis => Input.GetAxis("Horizontal");
@@ -16,12 +19,34 @@ public class PlayerCtrl : MonoBehaviour
 
     void Update()
     {
-        
+        Move();
+        Turn();
     }
 
     void Move()
     {
+        if(transform.position.z < -8.2 && vAxis < 0)
+            speedController = 0;
+        else if(transform.position.z > 8.2 && vAxis > 0)
+            speedController = 0;
+        else
+            speedController = 3f;
         moveVec = new Vector3(0, 0, vAxis);
-        transform.position += moveVec*Time.deltaTime;
+
+        transform.position += moveVec*speedController*Time.deltaTime;
+    }
+
+    void Turn()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        LayerMask layerMask = LayerMask.GetMask("Floor");
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            Vector3 newForward = hit.point - transform.position;
+            newForward.y = 0;
+            transform.rotation = Quaternion.LookRotation(newForward);
+        }
     }
 }
